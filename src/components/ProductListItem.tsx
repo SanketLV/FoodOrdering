0fb900110b897c5d0d4269711ffdfa149a228319
Sanket Lakhani <sanket.lakhani@dotnitron.com>
2024-03-02 17:47:20 +0530

    FlatList added

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -15,6 +15,7 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
         style={styles.image}
+        resizeMode="contain"
       />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
@@ -29,6 +30,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   title: {
     fontSize: 18,
