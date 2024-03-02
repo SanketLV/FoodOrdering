dd30278600a79655f4adf475205c81077d1b8e36
Sanket Lakhani <sanket.lakhani@dotnitron.com>
2024-03-02 16:59:56 +0530

    Your first custom component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,36 +1,12 @@
 import products from "@/assets/data/products";
-import Colors from "@/src/constants/Colors";
-import { StyleSheet, Text, View, Image } from "react-native";
+import ProductListItem from "@/src/components/ProductListItem";
+import { View } from "react-native";
 
-const product = products[1];
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-});
