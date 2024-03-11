9f72b84e5002ba32345726c90ab7a15cf01b6baa
Sanket Lakhani <sanket.lakhani@dotnitron.com>
2024-03-11 19:51:40 +0530

    Shopping Cart updated

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -2,21 +2,25 @@ import { FlatList, Platform, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { useCart } from "@/providers/CartProvider";
 import CartListItem from "@/components/CartListItem";
+import Button from "@/components/Button";
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
 
   return (
-    <View>
+    <View style={{ padding: 10 }}>
       <FlatList
         data={items}
         renderItem={({ item }) => (
           <CartListItem cartItem={item} key={item.id} />
         )}
-        contentContainerStyle={{ padding: 10, gap: 10 }}
+        contentContainerStyle={{ gap: 10 }}
       />
 
-      {/* <Text>Cart items length: {items.length}</Text> */}
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: "500" }}>
+        Total: ${total}
+      </Text>
+      <Button text="Checkout" />
     </View>
   );
 };
--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -6,12 +6,14 @@ type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
   updateQuantity: (itemId: string, amount: -1 | 1) => void;
+  total: number;
 };
 
 export const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
   updateQuantity: () => {},
+  total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -48,8 +50,13 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     );
   };
 
+  const total = items.reduce(
+    (sum, item) => (sum += item.product.price * item.quantity),
+    0
+  );
+
   return (
-    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
       {children}
     </CartContext.Provider>
   );
